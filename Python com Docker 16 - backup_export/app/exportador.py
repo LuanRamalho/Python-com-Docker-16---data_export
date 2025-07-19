c9d44b71c0f43c121e258be086c2f926373885dd
Luan Ramalho <32373552+LuanRamalho@users.noreply.github.com>
2025-07-19 16:31:20 -0300

    Delete Python com Docker 16 - backup_export directory

--- Python com Docker 16 - backup_export/app/exportador.py
+++
@@ -1,9 +0,0 @@
-import csv
-from database import obter_dados
-
-def exportar_csv(nome_arquivo="exportacao.csv"):
-    dados = obter_dados()
-    with open(nome_arquivo, mode='w', newline='', encoding='utf-8') as arquivo:
-        escritor = csv.writer(arquivo)
-        escritor.writerow(["ID", "Disciplina", "Horas", "Data"])
-        escritor.writerows(dados)
--- Python com Docker 16 - backup_export/app/main.py
+++
@@ -1,4 +0,0 @@
-from interface import executar_interface
-
-if __name__ == "__main__":
-    executar_interface()
